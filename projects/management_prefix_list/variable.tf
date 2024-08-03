88b2ce11cbbad15890ece626a5d7564ba3d83f0d
i9696lain <i9696lain@gmail.com>
2024-08-03 22:43:27 +0900

    add security

--- projects/management_prefix_list/variable.tf
+++ projects/management_prefix_list/variable.tf
@@ -2,3 +2,15 @@ variable "author" {
   default = "author"
 }
 
+variable "sample_cidrs_A" {
+  default = [
+    "172.64.1.1/32",
+  ]
+}
+
+variable "sample_cidrs_B" {
+  default = [
+    "172.64.1.2/32",
+    "172.64.1.3/32",
+  ]
+}
